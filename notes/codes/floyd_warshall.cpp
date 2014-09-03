fb69523623daa78ed78489b0da53fe6f80fc2853
Ondrej Budac <ondrob@gmail.com>
2014-09-03 16:36:41 +0200

    small changes

--- notes/codes/floyd_warshall.cpp
+++ notes/codes/floyd_warshall.cpp
@@ -4,8 +4,8 @@ int dist[N][N];
 // dist[i][j] = weight of the directed edge (i,j)
 // dist[i][j] = MAXINT if there is no edge (i,j)
 
-for(int k = 0; k < N; ++k)
-	for(int i = 0; i < N; ++i)
-		for(int j = 0; j < N; ++j)
+for (int k = 0; k < N; ++k)
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < N; ++j)
 			if (dist[i][j] > dist[i][k] + dist[k][j])
 				dist[i][j] = dist[i][k] + dist[k][j];
